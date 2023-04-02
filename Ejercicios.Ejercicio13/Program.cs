3edb813851a952977794891563073bb08a6a0257
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 16:19:02 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio13/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Ingrese dos frases para iniciar comparación");
+Console.WriteLine("Ingrese frase 1");
+string frasea = Console.ReadLine();
+Console.WriteLine("Ingrese frase 2");
+string fraseb = Console.ReadLine();
+
+if (frasea.Length == fraseb.Length)
+{
+    Console.WriteLine("Las dos frases son idénticas en longitud");
+}
+else
+{
+    Console.WriteLine("Las frases son distintas en longitud");
+}
